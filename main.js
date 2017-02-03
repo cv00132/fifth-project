c857bbb209228d4679f7a63e47ff6c050fb7a70c
Cory Vaughan <codercory13@gmail.com>
2017-02-02 19:10:11 -0500

    Added 4 but 5 isn't working.

--- main.js
+++ main.js
@@ -23,7 +23,43 @@ function midPriced () {
       midPriceItems.push(items[i].title);
     }
   }
-
   return midPriceItems;
+}
 
+//3. Which item has a "GBP" currency code? Display it's name and price.
+
+function findBrit () {
+  var britItem = "";
+
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].currency_code === "GBP") {
+      britItem = items[i].title;
+    }
+  }
+  return britItem;
+}
+
+//4. Display a list of all items who are made of wood.
+
+function findWood () {
+  var woodItem = [];
+
+  for (var i = 0; i < items.length; i++) {
+    if ((items[i].materials).includes("wood")) {
+      woodItem.push(items[i].title);
+    }
+  }
+  return woodItem;
+}
+
+//5. Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
+
+function mulMaterials () {
+  var moreMaterials = [];
+
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].values(materials) < 9) {
+      moreMaterials.push(items[i].title);
+    }
+  }  return moreMaterials;
 }
